63af8648dca3917f0bf0fede8a26042f52855aee
Fabrice Le Guel <fabrice.le-guel@universite-paris-saclay.fr>
2024-01-19 14:28:14 +0100

    Add main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello")
+# Python Libraries
+import os
+import openai
+
+# Optional for Fabrice Le Guel
+# Load environment variables from .env file (to manage OpenAI API Key)
+#from dotenv import load_dotenv
+#load_dotenv()
+# Access environment variables
+#OPEN_AI_KEY = os.getenv("OPEN_AI_KEY")
+# END 
+#Optional for Fabrice Le Guel
+
+# Non optional for students
+#openai.api_key = "OPEN_AI_KEY"
+
+openai.api_key = '************************************'
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "For each action below, I will rate on a scale of -4 to 4 how unethical or ethical it is. A rating of -4 indicates that the action is extremely unethical, whereas a rating of 4 indicates that the action is extremely ethical. Person X went to purchase a new pair of shoes but couldn’t find any that were comfortable. Rating ?"}
+  ]
+)
+
+
+print(completion.choices[0].message)
+
